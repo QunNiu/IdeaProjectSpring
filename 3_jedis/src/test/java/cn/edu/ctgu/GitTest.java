4c243a1bc462e1676eaedcc7abfc3c7ffea6bcfc
niuqun <NiuQun182@163.com>
2021-12-06 17:48:04 +0800

    master commit

--- 3_jedis/src/test/java/cn/edu/ctgu/GitTest.java
+++ 3_jedis/src/test/java/cn/edu/ctgu/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
         System.out.println("热修复完成");
+        System.out.println("master test");
     }
 }
